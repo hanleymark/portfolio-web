ff71969472d9fdacc402c2b726e0ae782219b2f9
hanleymark <home@markhanley.co.uk>
2023-09-20 17:59:11 +0100

    Set up component structure stubs

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import Navbar from '@/components/navbar'
+import Image from 'next/image';
+import Link from 'next/link';
+import Navbar from '@/components/navbar';
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Navbar />
-    </main>
-  )
+      <main>
+
+      </main>
+    </>
+  );
 }
---
+++ components/about.tsx
@@ -0,0 +1,7 @@
+const About: React.FC = () => {
+  return (
+    <div className="w-screen h-screen bg-gray-500">
+      <h1 className="text-white text-4xl">About</h1>
+    </div>
+  );
+};
