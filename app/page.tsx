411f51ff786ac58843c3d03ca7b33c732da49181
hanleymark <home@markhanley.co.uk>
2023-09-20 15:33:37 +0100

    Add NavButton and NavBar components and basic styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import Navbar from '@/components/navbar'
 
 export default function Home() {
   return (
     <main>
-      
+      <Navbar />
     </main>
   )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+"use client"
 
-const Navbar = () => {
-    return (
-        <header>
-            <div className="navContainer">
-                Navbar
-            </div>
-        </header>
-    );
-}
+import React from 'react';
+import { useState } from 'react';
+import NavButton from './navButton';
+
+const Navbar: React.FC = () => {
+const buttons = [
+    {
+        content: 'About',
+        link: '#',
+    },
+    {
+        content: 'Projects',
+        link: '#',
+    },
+    {
+        content: 'Contact',
+        link: '#',
+    },
+];
+
+const [activeButton, setActiveButton] = useState<string>(buttons[0].content);
+
+  return (
+    <header>
+      <nav className='w-screen bg-gray-600 h-auto flex justify-center space-x-5'>
+        {buttons.map((button, index) => {
+            return (
+                <NavButton
+                    key={index}
+                    content={button.content}
+                    link={button.link}
+                    isActive={activeButton === button.content}
+                    onClick={() => setActiveButton(button.content)}
+                />
+            );
+        })}
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
