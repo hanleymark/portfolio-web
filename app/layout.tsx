6f94747e1f559ad84299c096f9f7e6886084a071
Mark hanley <home@markhanley.co.uk>
2023-09-27 14:06:30 +0100

    Fix misapplied line height variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' theme='dark'>
       <body>{children}</body>
     </html>
   );
