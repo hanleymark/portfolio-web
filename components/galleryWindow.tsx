7e7687a4ba5fa6c8f4ac9baa8d6bd7423aff9058
Mark hanley <home@markhanley.co.uk>
2023-09-29 14:52:42 +0100

    Initial tailwind styling

--- components/galleryWindow.tsx
+++ components/galleryWindow.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cntl from 'cntl';
 
 interface Props {
   title: string;
@@ -8,10 +9,27 @@ interface Props {
   description: string;
 }
 
+const windowTW = cntl`
+  w-full
+  aspect-4/3
+  h-auto
+  rounded-md
+  bg-slate-300
+  shadow-md
+  md:w-full
+  lg:w-1/2
+  xl:w-1/3
+  2xl:w-1/4
+  flex
+`;
+
 const GalleryWindow = (props: Props) => {
   return (
-    <div className='aspect-4/3 h-auto w-full w-full rounded-md bg-slate-300 p-2 shadow-md sm:w-1/2 md:w-1/3 lg:w-1/4'>
-      Hello
+    <div className={windowTW}>
+      <div className='w-max flex-row flex-nowrap gap-1'>
+        <div className='w-max basis-1/6'>Left</div>
+        <div className='w-max basis-5/6 bg-white'>Right</div>
+      </div>
     </div>
   );
 };
--- components/projectGallery.tsx
+++ components/projectGallery.tsx
@@ -8,7 +8,21 @@ const ProjectGallery = ({ heading }: Props) => {
   return (
     <section id={'gallery'}>
       {heading && <h3>{heading}</h3>}
-      <div className='flex flex-wrap'>
+      <div className='flex flex-wrap flex-row gap-5 justify-center'>
+        <GalleryWindow
+          title='A sample project'
+          image='./projects/pic.png'
+          link='#'
+          alt='A project image'
+          description='This is a description'
+        />
+        <GalleryWindow
+          title='A sample project'
+          image='./projects/pic.png'
+          link='#'
+          alt='A project image'
+          description='This is a description'
+        />
         <GalleryWindow
           title='A sample project'
           image='./projects/pic.png'
